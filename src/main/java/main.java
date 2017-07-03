c6b518686adc1ffa9e9025ec8cfb240daeb79fe8
karolina <kajapestova@seznam.cz>
2017-07-03 16:26:27 +0200

    zmena textu vykricniky + ignorujeme projekt ideii

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,10 @@
 /**
+ *
  * Created by Karolínka on 2.7.2017.
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello all people in the world");
+        System.out.println("hello!!!!! all people in the world");
     }
 }
 
